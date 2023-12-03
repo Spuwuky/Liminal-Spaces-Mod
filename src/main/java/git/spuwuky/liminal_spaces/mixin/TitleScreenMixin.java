8fc27e2311902d0781fdc23427282d4cbc7f269b
Spuwuky <99519961+Spuwuky@users.noreply.github.com>
2023-12-02 19:44:29 -0600

    Add files via upload

---
+++ src/main/java/git/spuwuky/liminal_spaces/mixin/TitleScreenMixin.java
@@ -0,0 +1,16 @@
+package git.spuwuky.liminal_spaces.mixin;
+
+import git.spuwuky.liminal_spaces.LiminalSpaces;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class TitleScreenMixin {
+	@Inject(method = "init", at = @At("TAIL"))
+	public void onInit(CallbackInfo ci) {
+		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
