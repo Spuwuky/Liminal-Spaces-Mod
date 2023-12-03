849f360f8eb57342ec49340317347f9e3b6ce8b5
Spuwuky <99519961+Spuwuky@users.noreply.github.com>
2023-12-02 19:38:38 -0600

    Add files via upload

---
+++ src/main/java/git/spuwuky/SPOOKYCOREDataGenerator.java
@@ -0,0 +1,11 @@
+package git.spuwuky;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SPOOKYCOREDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
